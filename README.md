441e48b83b674ba22efcebea138a961fb332c27a
Attila Simon <simon.attila.fr@gmail.com>
2025-01-09 00:45:16 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a [Vite](https://vite.dev/) project bootstrapped with `create-vite`, using react template.
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <p>Navbar</p>
+      <p>Page content</p>
+    </>
+  )
+}
+
+export default App
